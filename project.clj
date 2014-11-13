1f03bb2f4388a2d34c070ccaa26ca2b6552de103
David Conner <dconner.pro@gmail.com>
2014-11-13 18:49:52 -0500

    * set up dependencies in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject gpu-genomics "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/dcunited001/gpu-genomics"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :resource-paths ["/opt/Aparapi_2012_01_23_MacOSX_zip/aparapi.jar"]
+  :jvm-opts ["-Djava.library.path=/opt/Aparapi_2012_01_23_MacOSX_zip"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [aparapi-matrix "0.1.0-SNAPSHOT"]])
